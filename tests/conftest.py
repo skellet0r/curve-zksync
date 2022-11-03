4711be2c1ab350abb85fc727888a27d93133d206
Edward Amor <edward.amor3@gmail.com>
2022-11-02 22:06:39 -0400

    fix: raise error when devnet is not found for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,23 @@
 import pathlib
 
 import pytest
+import requests
 from ape_zksync.account import TestAccount, TestAccountContainer
 
 BASE_DIR = pathlib.Path(__file__).parent.parent.absolute()
+
+
+def pytest_sessionstart(session):
+    try:
+        response = requests.post(
+            "http://localhost:3050",
+            json={"jsonrpc": "2.0", "method": "web3_clientVersion", "params": [], "id": 0},
+        )
+        assert response.json()["result"] == "zkSync/v2.0"
+    except Exception:
+        raise RuntimeError(
+            "zkSync v2 dev network was not found locally at 'http://localhost:3050'"
+        ) from None
 
 
 @pytest.fixture(scope="session")
