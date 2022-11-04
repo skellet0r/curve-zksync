9dd57d921d8ac89d9b666b83479b55dd7f315757
Edward Amor <edward.amor3@gmail.com>
2022-11-04 14:18:31 -0400

    test: AddressProvider add_new_id fn

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -10,3 +10,19 @@ def test_constructor(alice, provider):
     assert provider.admin() == alice
     assert provider.max_id() == 0
     assert provider.get_id_info(0).description == "Main Registry"
+
+
+def test_add_new_id(alice, chain, provider):
+    receipt = provider.add_new_id(provider, "Foo", sender=alice)
+    assert provider.max_id() == 1
+
+    info = provider.get_id_info(1)
+    assert info.addr == provider
+    assert info.is_active is True
+    assert info.version == 1
+    # TODO: TIMESTAMP opcode is 2 seconds earlier than timestamp returned by eth_getBlockBy*
+    # assert info.last_modified == chain.blocks[receipt.block_number].timestamp
+    assert info.description == "Foo"
+
+    event = next(provider.NewAddressIdentifier.from_receipt(receipt))
+    assert event.event_arguments == dict(id=1, addr=provider.address, description="Foo")
