df2c598d40042fa04253f1c28292269d339811ad
Edward Amor <edward.amor3@gmail.com>
2022-11-04 15:01:13 -0400

    test: set address (registry)

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -21,8 +21,7 @@ def test_add_new_id(alice, chain, mock_foo, provider):
     assert info.addr == mock_foo
     assert info.is_active is True
     assert info.version == 1
-    # TODO: TIMESTAMP opcode is 2 seconds earlier than timestamp returned by eth_getBlockBy*
-    # assert info.last_modified == chain.blocks[receipt.block_number].timestamp
+    assert info.last_modified == chain.blocks[receipt.block_number].timestamp
     assert info.description == "Foo"
 
     event = next(provider.NewAddressIdentifier.from_receipt(receipt))
@@ -37,3 +36,18 @@ def test_add_new_id_reverts_invalid_caller(bob, mock_foo, provider):
 def test_add_new_id_reverts_invalid_address(alice, bob, provider):
     with ape.reverts():
         provider.add_new_id(bob, "Foo", sender=alice)
+
+
+def test_set_address_registry(alice, chain, mock_foo, provider):
+    receipt = provider.set_address(0, mock_foo, sender=alice)
+    assert provider.max_id() == 0  # unchanged
+    assert provider.get_registry() == mock_foo
+
+    info = provider.get_id_info(0)
+    assert info.addr == mock_foo
+    assert info.is_active is True
+    assert info.version == 1
+    assert info.last_modified == chain.blocks[receipt.block_number].timestamp
+
+    event = next(provider.AddressModified.from_receipt(receipt))
+    assert event.event_arguments == dict(id=0, new_address=mock_foo.address, version=1)
