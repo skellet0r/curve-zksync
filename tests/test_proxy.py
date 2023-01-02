33b4ba1b10b43e3a896be557032dd4b96f46040a
Edward Amor <edward.amor3@gmail.com>
2023-01-02 17:07:33 -0500

    test: ProxyAdmin.approve_admin_change

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -37,3 +37,26 @@ def test_request_admin_change_reverts_already_admin(proxy, alice, bob):
 def test_request_admin_change_reverts_caller_not_admin(proxy, charlie):
     with ape.reverts():
         proxy.request_admin_change(charlie, sender=charlie)
+
+
+def test_approve_admin_change(proxy, alice, bob, charlie):
+    proxy.request_admin_change(charlie, sender=alice)
+    receipt = proxy.approve_admin_change(sender=bob)
+
+    assert proxy.change_approved() is True
+
+    event = next(proxy.ApproveAdminChange.from_receipt(receipt))
+    assert event.event_arguments == dict(
+        current_admin=alice, future_admin=charlie, calling_admin=bob
+    )
+
+
+def test_approve_admin_change_reverts_no_active_request(proxy, alice):
+    with ape.reverts():
+        proxy.approve_admin_change(sender=alice)
+
+
+def test_approve_admin_change_reverts_invalid_caller(proxy, alice, charlie):
+    proxy.request_admin_change(charlie, sender=alice)
+    with ape.reverts():
+        proxy.approve_admin_change(sender=alice)
